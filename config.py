79f56eb914b78e3d7a2bb53939ff70f3771b9c6c
jinsung Park <dksk0101@gmail.com>
2023-04-13 18:25:10 +0900

    리프레쉬 토큰 기간

--- config.py
+++ config.py
@@ -7,18 +7,19 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    BASE_DIR: str = path.dirname((path.abspath(__file__))) # 기본 디렉토리 위치
-    # 개발할때 로컬에서 확인
-    LOCAL_MODE: bool = True if system().lower().startswith("darwin") or system().lower().startswith("Windows") else False
+    BASE_DIR: str = path.dirname((path.abspath(__file__)))
+    LOCAL_MODE: bool = (
+        True if system().lower().startswith("darwin") or system().lower().startswith("Windows") else False
+    )
     app_name: str = "Imizi API"
     TEST_MODE: bool = False
-    # 어떤 프론트엔드에서 접속을 허용할거냐
+
     ALLOW_SITE = ["*"]
-    # 해당 서버가 해당 도메인으로 접속
     TRUSTED_HOSTS = ["*"]
     JWT_ALGORITHM = "HS256"
     JWT_SECRET_KEY = os.getenv("JWT_SECRET", "imizi-secret")
     ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 1  # one day
+    REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 60  # sixty day
 
     DB_URL: str = ""
     DB_POOL_RECYCLE: Optional[int] = 900
